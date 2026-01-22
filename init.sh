73970e22d6b46c02b9ad17bc63e0b5b0757998bc
738NGX <jny738ngx@gmail.com>
2026-01-22 23:33:13 +0800

    更新init

---
+++ init.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+pip install -r requirements.txt
+
+# Download and extract Qlib data
+wget https://github.com/chenditc/investment_data/releases/latest/download/qlib_bin.tar.gz
+mkdir -p ~/.qlib/qlib_data/cn_data
+tar -zxvf qlib_bin.tar.gz -C ~/.qlib/qlib_data/cn_data --strip-components=1
+rm -f qlib_bin.tar.gz
