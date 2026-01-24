4d041424712e6004d125bf84a9d8cfc4aa987d3c
738NGX <jny738ngx@gmail.com>
2026-01-24 21:11:41 +0800

    更新

--- init.sh
+++ init.sh
@@ -3,7 +3,7 @@ pip install -r requirements.txt
 pt update && apt install fonts-noto-cjk
 
 # Download and extract Qlib data
-wget https://github.com/chenditc/investment_data/releases/latest/download/qlib_bin.tar.gz
-mkdir -p ~/.qlib/qlib_data/cn_data
-tar -zxvf qlib_bin.tar.gz -C ~/.qlib/qlib_data/cn_data --strip-components=1
-rm -f qlib_bin.tar.gz
+# wget https://github.com/chenditc/investment_data/releases/latest/download/qlib_bin.tar.gz
+# mkdir -p ~/.qlib/qlib_data/cn_data
+# tar -zxvf qlib_bin.tar.gz -C ~/.qlib/qlib_data/cn_data --strip-components=1
+# rm -f qlib_bin.tar.gz
